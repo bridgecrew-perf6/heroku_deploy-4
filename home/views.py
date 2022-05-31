825c2aae8ecce3d475e6bac19a766f73ca6cf86d
Emmanuel Chijioke Ndubuisi <emmaaustin509@gmail.com>
2022-05-31 20:06:41 +0100

    made changes to to the home function

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('welcome home')
+    return HttpResponse('welcome home and that is all')
